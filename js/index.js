5c6923ceec95fab5123d7f7dc58a51aa8a396083
PrernaPardhy <prerna.pardhy@gmailcom>
2018-12-30 08:24:34 +0530

    All features are added in PartA

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function funSignUp(){
+    
+    document.getElementById('myModal').style.display='block';
+}
+
+function funSignIn(){
+    document.getElementById('myModal1').style.display='block';
+}
+
+function funCreatePost(){
+    document.getElementById('myModal2').style.display='block';
+}
